aa67a8982ec7975cd92e3c3d780e4ba63d6279f9
forstuffapps <88136654+forstuffapps@users.noreply.github.com>
2021-10-27 00:31:50 +0530

    Add files via upload

---
+++ Internet plag 2.py
@@ -0,0 +1,33 @@
+import collections
+
+def qwe():
+    q,w,e,r = 0,0,0,0
+    if 'r'=='app':
+        return 'y lk app'
+    elif 't'=='cr':
+        return 'sv pl'
+    else:
+        return 'n0 crs'
+    
+    for 'r' in 'wr':
+        for 't' in 'sf':
+            print('nt th')
+            
+            if q==r and q in r:
+                return q=w+e+r*r+e*w
+            elif w==e and q*w+r==e:
+                return q*e*r+w*e+r*q-r*w
+            else:
+                return 'qw*e'
+    
+    fl=False
+    fl=(q*w+e-r*q)==(q-1*e+3*r+4*q)
+    
+    
+def asd():
+    z,x,c,v = 0,10,2,56
+    
+    if z*x+c-v==45:
+        return z+c-x*z-c*v+z*v
+    else:
+        return c*x-c*v+c*c
